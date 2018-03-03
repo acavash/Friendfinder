c54096005ff807c71be9a2a433f67f0e91d10bea
Avash Chapagain <Avash@Avashs-MacBook-Pro.local>
2018-03-03 08:31:17 -0500

    First Commit

---
+++ app/apiRoutes.js
@@ -0,0 +1,62 @@
+// Load Data
+// linking our routes to a series of data sources.
+var friends_data = require('../data/friends.js');
+
+
+// Routing
+module.exports = function(app) {
+
+	// API GET requests
+	// link to the data stored in our app so that users can see the JSON data when they visit the below link
+	app.get('/api/friends', function(req, res) {
+		res.json(friends_data);
+	});
+
+	// API POST requests
+	// below handles the user submitted form. In this app's case, the friend data to match that user to others that are in the data js files.
+	app.post('/api/friends', function(req, res) {
+		var body = req.body;
+		console.log('post received')
+		console.log(body)
+		// body.scores is the scores array of new user
+		// var a1 = [5,5,3,1,1,2,5,5,3,5]
+
+		var outside = [];
+		var outside2 = [];
+
+		for(var k = 0; k < friends_data.length; k++) {
+		var inside = []
+		for (var i = 0; i < body.scores.length; i++) {
+		var minus = Math.abs(friends_data[k].scores[i] - body.scores[i])
+		inside.push(minus)
+		}
+		outside2.push(inside)
+		}
+
+		// console.log(outside2)
+		for (var i = 0; i < outside2.length; i++) {
+			// console.log(outside2[i])
+			var sum2 = outside2[i].reduce(function(all,item,index) {
+			return all+=item
+			}, 0)
+			// console.log(sum2)
+			outside.push(sum2)
+		}
+		//outside is the sum of difference comparing the new score with the friends data
+		console.log(outside)
+		//index is a number that will change if the value is larger than
+		var index = 0, lowest = outside[0]
+		for (var i = 0; i < outside.length; i++) {
+			if (outside[i] < lowest) {
+				lowest = outside[i]
+				index = i
+			}
+		}
+		console.log(index)
+		console.log(lowest)
+
+		res.json(friends_data[index])
+
+
+
+	});
---
+++ app/data/friends.js
@@ -0,0 +1,33 @@
+// Data of users in the system
+var friendsArray = [
+
+	{
+		name: 'Bernie Sanders',
+		photo: 'http://spectrum.ieee.org/ns/slideshows/03SS_MyDigitalEye_2a/fullscreen/d.jpg',
+		scores: [1,1,1,1,1,1,1,1,1,1]
+	},
+	{
+		name: 'Drake',
+		photo: 'https://s-media-cache-ak0.pinimg.com/564x/a6/69/a4/a669a44bb260890936983c2b437620c9.jpg',
+		scores: [2,2,2,2,2,2,2,2,2,2]
+	},
+	{
+		name: 'Kanye West',
+		photo: 'https://cbsmiami.files.wordpress.com/2016/06/christina-grimmie.jpg?w=625&h=352&crop=1',
+		scores: [3,3,3,3,3,3,3,3,3,3]
+	},
+	{
+		name: 'Justin Beiber',
+		photo: 'https://d2yy7txqjmdbsq.cloudfront.net/fundraiserevents/193626f7-de8b-4508-994f-1c39b3ac8ce2/logo_10253862_10152829045056794_7617969505543251711_n.jpg',
+		scores: [4,4,4,4,4,4,4,4,4,4]
+	},
+	{
+		name: 'Bob Ross',
+		photo: 'http://chano8.com/wp-content/uploads/2015/09/b-o-b.jpg',
+		scores: [5,5,5,5,5,5,5,5,5,5]
+	}
+
+]
+
+// exporting so it's available to other files
+module.exports = friendsArray;
